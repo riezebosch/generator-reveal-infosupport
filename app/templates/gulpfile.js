dde26a8ed4ab4354bd31cd3efb7c30b517e99dcf
Manuel Riezebosch <mriezebosch@gmail.com>
2017-03-17 23:19:18 +0100

    serve searches for open ports

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -4,29 +4,35 @@ var template = require('gulp-template')
 var dirTree = require('directory-tree')
 var watch = require('gulp-watch')
 var batch = require('gulp-batch')
-var server = require('gulp-webserver');
+var connect = require('gulp-connect');
+var portastic = require('portastic')
+var open = require('opn')
 
 gulp.task('serve', ['build', 'watch'], function() {
-  gulp.src('.')
-    .pipe(server({
-      livereload: true,
-      open: true,
-      port: 9000
-    }));
+    portastic
+        .find({ min: 9000, max: 9999, retrieve: 2 })
+        .then(function(port) {
+            open('http://localhost:' + port[0] + '/')
+            connect.server({
+                root: '.',
+                port: port[0],
+                livereload: { port: port[1] }
+            });
+        })
 });
 
 gulp.task('print', ['build'], function() {
-    // construct file path url for phantomjs
     var input = 'file:///' + process.cwd() + '/index.html?print-pdf';
     spawn('phantomjs', ['bower_components/reveal.js/plugin/print-pdf/print-pdf.js', input, 'slides.pdf'], { stdio: 'inherit'});
 });
 
 gulp.task('build', function() {
-  var slides = dirTree('slides')
-  gulp
-    .src('templates/index.html')
-    .pipe(template({ data: slides }))
-    .pipe(gulp.dest('.'))
+    var slides = dirTree('slides')
+    gulp
+        .src('templates/index.html')
+        .pipe(template({ data: slides }))
+        .pipe(gulp.dest('.'))
+        .pipe(connect.reload())
 });
 
 gulp.task('watch', function () {
